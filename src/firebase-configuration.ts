7d3233b9a8d170895baf3995add4d2964c870192
ramadhanialfarizi <ramadhani.alfarizi@gmail.com>
2025-11-04 22:00:33 +0700

    configuration firebase

---
+++ src/firebase-configuration.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDGlxgAlJJmvTBI6PKDhexF9Q_Sae85o2U",
+  authDomain: "stomata-blockchain.firebaseapp.com",
+  projectId: "stomata-blockchain",
+  storageBucket: "stomata-blockchain.firebasestorage.app",
+  messagingSenderId: "154029402657",
+  appId: "1:154029402657:web:a7c0d1bfc2653df4e95160"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
