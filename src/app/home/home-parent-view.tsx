9eaa1b1a02a952aa3cc9c44aea4d893ef1567a90
ramadhanialfarizi <ramadhani.alfarizi@gmail.com>
2025-11-06 21:58:59 +0700

    add signout

--- src/app/home/home-parent-view.tsx
+++ src/app/home/home-parent-view.tsx
@@ -1,7 +1,12 @@
 import {
   AppBar,
   Box,
+  Button,
   CssBaseline,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
   Divider,
   Drawer,
   List,
@@ -21,6 +26,9 @@ import { colorPalette } from "../../theme/color-palette";
 import { useState, type JSX } from "react";
 import { useNavigate } from "react-router-dom";
 
+import { signOut } from "firebase/auth";
+import { auth } from "../../firebase-configuration";
+
 const drawerWidth = 240;
 
 interface MenuItem {
@@ -30,8 +38,6 @@ interface MenuItem {
 }
 
 export default function HomeParentView() {
-  const navigate = useNavigate();
-
   const menus: MenuItem[] = [
     {
       icon: <AgricultureIcon sx={{ color: colorPalette.primary.darkGreen }} />,
@@ -58,16 +64,44 @@ export default function HomeParentView() {
   ];
 
   const [selected, setSelected] = useState<string>("");
+  const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
+  const [isLoading, setLoading] = useState(false);
+  const [isError, setError] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
+  const [disableButton, setDisableButton] = useState(false);
 
   const handleClick = (menu: MenuItem, index: number) => {
     setSelected(menu.name);
     console.log("Clicked:", menu.name);
 
     if (menu.type === "SIGNOUT") {
-      navigate(-1);
+      // navigate(-1);
+      handleOpen();
     }
     // bisa tambah navigasi atau aksi lain di sini
   };
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  async function logout() {
+    setLoading(true);
+    setDisableButton(true);
+    await signOut(auth)
+      .then(() => {
+        navigate(-1);
+        handleClose();
+      })
+      .catch((error) => {
+        setError(true);
+        setErrorMsg(error.message);
+        handleClose();
+      });
+
+    setLoading(false);
+    setDisableButton(false);
+  }
 
   return (
     <Box sx={{ display: "flex" }}>
@@ -110,41 +144,53 @@ export default function HomeParentView() {
           ))}
         </List>
       </Drawer>
-      {/* <Box
-        component="main"
-        sx={{ flexGrow: 1, bgcolor: "background.default", p: 3 }}
+
+      {/* Body content letakan  sini */}
+
+      <Dialog
+        disableEscapeKeyDown
+        open={open}
+        onClose={(event, reason) => {
+          if (reason === "backdropClick") return; // cegah close saat klik background
+          handleClose();
+        }}
       >
-        <Toolbar />
-        <Typography sx={{ marginBottom: 2 }}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-          dolor purus non enim praesent elementum facilisis leo vel. Risus at
-          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
-          quisque non tellus. Convallis convallis tellus id interdum velit
-          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
-          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
-          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
-          faucibus et molestie ac.
-        </Typography>
-        <Typography sx={{ marginBottom: 2 }}>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
-          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
-          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
-          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
-          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
-          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
-          morbi tristique senectus et. Adipiscing elit duis tristique
-          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
-      </Box> */}
+        <DialogTitle>Logout ?</DialogTitle>
+        <DialogContent>
+          <Typography>Are you sure want to logout?</Typography>
+          {isError && (
+            <p
+              style={{
+                color: colorPalette.primary.error,
+              }}
+            >
+              {errorMsg}
+            </p>
+          )}
+        </DialogContent>
+        <DialogActions>
+          <Button
+            disabled={disableButton}
+            onClick={handleClose}
+            sx={{
+              color: colorPalette.primary.darkGreen,
+            }}
+          >
+            Cancel
+          </Button>
+          <Button
+            loading={isLoading}
+            onClick={() => {
+              logout();
+            }}
+            sx={{
+              color: colorPalette.primary.error,
+            }}
+          >
+            Logout
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Box>
   );
 }
