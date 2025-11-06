798cfa9850b769a73814ac60ff751094b5ebd500
ramadhanialfarizi <ramadhani.alfarizi@gmail.com>
2025-11-06 20:34:05 +0700

    update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,14 +2,14 @@ import { useRoutes } from "react-router-dom";
 import { SplashScreenRoutes } from "./section/splash-screen";
 import { AuthSigninRoutes } from "./section/auth-signin";
 import { AuthSignupRoutes } from "./section/auth-signup";
-import { HomeViewRoutes } from "./section/home-view";
+import { HomeParentViewRoutes } from "./section/home-parent-view";
 
 function AppRoutes() {
   const routes = useRoutes([
     ...SplashScreenRoutes,
     ...AuthSigninRoutes,
     ...AuthSignupRoutes,
-    ...HomeViewRoutes,
+    ...HomeParentViewRoutes,
   ]);
 
   return routes;
--- src/routes/section/home-parent-view.tsx
+++ src/routes/section/home-parent-view.tsx
@@ -1,7 +1,7 @@
 import type { RouteObject } from "react-router-dom";
 import HomeParentView from "../../app/home/home-parent-view";
 
-export const HomeViewRoutes: RouteObject[] = [
+export const HomeParentViewRoutes: RouteObject[] = [
   {
     path: "/home",
     element: <HomeParentView />,
